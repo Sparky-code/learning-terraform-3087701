a385f0a2a344bd5773f2f0e8018d1a67b3accab6
dkrizwold <dkrizwold@linkedin.com>
2024-06-14 15:11:21 -0700

    update for azure

--- providers.tf
+++ providers.tf
@@ -1,11 +1,12 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
     }
   }
 }
 
-provider "aws" {
-  region  = "us-west-2"
+provider "azurerm" {
+  features {}
 }
