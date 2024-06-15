596e238f75b6f652c0a609566bf7564fb1cc96ed
dkrizwold <dkrizwold@linkedin.com>
2024-06-14 18:02:58 -0700

    enable pw auth

--- main.tf
+++ main.tf
@@ -43,6 +43,7 @@ resource "azurerm_linux_virtual_machine" "example" {
   network_interface_ids = [
     azurerm_network_interface.example.id,
   ]
+  disable_password_authentication = false
 
   os_disk {
     caching              = "ReadWrite"
