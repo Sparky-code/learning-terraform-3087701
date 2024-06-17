417b4c00c949cb1629f0b11aa3abe698872618ef
dkrizwold <dkrizwold@linkedin.com>
2024-06-17 10:09:28 -0700

    understanding resource naming

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "azurerm_network_interface" "example" {
 }
 
 resource "azurerm_linux_virtual_machine" "example" {
-  name                = "web"
+  name                = "example-vm"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   size                = var.vm_size
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "source_image_id" {
-    value = "${azurerm_linux_virtual_machine.web.source_image_reference.publisher}:${azurerm_linux_virtual_machine.web.source_image_reference.offer}:${azurerm_linux_virtual_machine.web.source_image_reference.sku}:${azurerm_linux_virtual_machine.web.source_image_reference.version}"
+    value = "${azurerm_linux_virtual_machine.example.source_image_reference.publisher}:${azurerm_linux_virtual_machine.example.source_image_reference.offer}:${azurerm_linux_virtual_machine.example.source_image_reference.sku}:${azurerm_linux_virtual_machine.example.source_image_reference.version}"
 }
 
 output "source_id" {
-    value = azurerm_linux_virtual_machine.web.id
+    value = azurerm_linux_virtual_machine.example.id
 }
